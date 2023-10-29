2b629dcdcb2bc8f75eb50ce74d8222d1e3fa489e
Charlie Coppinger <charles.coppinger@gmail.com>
2023-10-30 12:07:00 +1300

    pre-shadcn

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -69,14 +69,7 @@ ooooooooooooo oooo                         .oooooo.                             
      888       888   888  888ooo888      888     ooooo  .oP"888   888   888   888   888   888  888  888ooo888  888     `"Y88b.       888     ooooo  888   888   888   888  888   888  
      888       888   888  888    .o      `88.    .88'  d8(  888   888   888   888   888   888  888  888    .o  888     o.  )88b      `88.    .88'   888   888   888   888  888   888  
     o888o     o888o o888o `Y8bod8P'       `Y8bood8P'   `Y888""8o o888o o888o o888o  `Y8bod8P' o888o `Y8bod8P' d888b    8""888P'       `Y8bood8P'    `V88V"V8P' o888o o888o `Y8bod88P" 
-                                                                                                                                                                                      
-                                                                                                                                                                                      
-                                                                                                                                                                                      
-                                                                                                                                             
-                                                                                                                                             
-                                                                                                                                             
     </pre>
-
     <div class="flex flex-col gap-8 items-center">
         <p>Making small bets is hard—don’t do it alone</p>
         <p class="text-gray-300">
@@ -85,20 +78,132 @@ ooooooooooooo oooo                         .oooooo.                             
         </p>
     </div>
 
-    <div class="flex gap-8 justify-center max-w-screen-md">
+    <div class="flex gap-8 justify-center max-w-screen-md items-center">
         <div class="flex flex-col gap-4 items-center">
+            <pre
+                style="white-space: pre;
+  font-family: monospace;
+  font-size: 3px;
+  line-height: 3px;
+  letter-spacing: unset;
+  transform: unset;
+  overflow-y: hidden;">                                                                                          
+             AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                     
+           AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                  
+          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                 
+        AAAAAAAAA AAAAAAAAA AAAAAAAAA AAAAAAAAAAAAAAAAAAAA                 
+     AAAAAAAAAA     AAAAA     AAAAA     AAAAAAAAAAAAAAAAAA                 
+      AAAAAAAAAA   AAAAAAA   AAAAAAA   AAAAAAAAAAAAAAAAAAA                 
+          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                 
+           AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                  
+            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                    
+                                                                           
+                                                                           
+                  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA               
+                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA             
+               AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA            
+            AAAAAAAAA   AAAAAAA   AAAAAAA   AAAAAAA   AAAAAAAAA            
+          AAAAAAAAAA     AAAAA     AAAAA     AAAAA     AAAAAAAA            
+            AAAAAAAAA   AAAAAAA   AAAAAAA   AAAAAAA   AAAAAAAAA            
+               AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA            
+                AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA             
+                  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA                
+                                                                           
+                                                                           
+                      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA          
+                     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA       
+                AAAAAAAAAA   AAAAAAA   AAAAAAAAAAAAAAAAAAAAAAAAAAAAA       
+               AAAAAAAAAA     AAAAA     AAAAAAAAAAAAAAAAAAAAAAAAAAAA       
+                  AAAAAAAAA AAAAAAAAA AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA       
+                    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA       
+                     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        
+                       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA           
+</pre>
             <p>⚀ Forum</p>
             <p class="text-gray-300">
                 A message board for weekly check-ins and long-form discussions
             </p>
         </div>
         <div class="flex flex-col gap-4 items-center">
+            <pre
+                style="white-space: pre;
+  font-family: monospace;
+  font-size: 3px;
+  line-height: 3px;
+  letter-spacing: unset;
+  transform: unset;
+  overflow-y: hidden;">                                                                                          
+                         AAAAA               AAAAA                         
+                  AAAAAAAAAAAAA             AAAAAAAAAAAAA                  
+              AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA              
+            AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA            
+          AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA          
+         AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA         
+         AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA         
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        
+       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA       
+      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      
+      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      
+     AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA     
+     AAAAAAAAAAAAAAAAAAAA     AAAAAAAAAAAAAAA     AAAAAAAAAAAAAAAAAAAA     
+    AAAAAAAAAAAAAAAAAAAA       AAAAAAAAAAAAA       AAAAAAAAAAAAAAAAAAAA    
+    AAAAAAAAAAAAAAAAAAA         AAAAAAAAAAA         AAAAAAAAAAAAAAAAAAA    
+    AAAAAAAAAAAAAAAAAAA         AAAAAAAAAAA         AAAAAAAAAAAAAAAAAAA    
+   AAAAAAAAAAAAAAAAAAAA         AAAAAAAAAAA         AAAAAAAAAAAAAAAAAAAA   
+   AAAAAAAAAAAAAAAAAAAA         AAAAAAAAAAA         AAAAAAAAAAAAAAAAAAAA   
+   AAAAAAAAAAAAAAAAAAAAA       AAAAAAAAAAAAA       AAAAAAAAAAAAAAAAAAAAA   
+  AAAAAAAAAAAAAAAAAAAAAAAA   AAAAAAAAAAAAAAAAA   AAAAAAAAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA  
+   AAAAAAAAAAAAAAAA       AAAAAAAAAAAAAAAAAAAAAAA       AAAAAAAAAAAAAAAA   
+    AAAAAAAAAAAAAAAA                                   AAAAAAAAAAAAAAAA    
+      AAAAAAAAAAAAAAAA                              AAAAAAAAAAAAAAAAA      
+        AAAAAAAAAAAAAAAAAA                       AAAAAAAAAAAAAAAAA         
+            AAAAAAAAAAAAA                         AAAAAAAAAAAAA            
+                   AAAA                             AAAA                   
+</pre>
             <p>⚁ Discord</p>
             <p class="text-gray-300">
                 A Discord server for feedback and help requests
             </p>
         </div>
         <div class="flex flex-col gap-4 items-center">
+            <pre
+                style="white-space: pre;
+  font-family: monospace;
+  font-size: 3px;
+  line-height: 3px;
+  letter-spacing: unset;
+  transform: unset;
+  overflow-y: hidden;">                                                                                          
+      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        
+  AA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AA  
+  AAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAA  
+  AAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAA  
+  AAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAA  
+  AAAAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAAAA  
+  AAAAAAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAAAAAA  
+  AAAAAAAAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAAAA      AAAAAAAAAAAAAAAAAAA      AAAAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAAAA        AAAAAAAAAAAAAAA        AAAAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAAAA            AAAAAAAAAAA            AAAAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAAAA      AAAA      AAAAAAA      AAAA      AAAAAAAAAAAAAAAA  
+  AAAAAAAAAAAAAA       AAAAAA                 AAAAAA       AAAAAAAAAAAAAA  
+  AAAAAAAAAAAAA      AAAAAAAAAA             AAAAAAAAAA      AAAAAAAAAAAAA  
+  AAAAAAAAAAA      AAAAAAAAAAAAAAAA     AAAAAAAAAAAAAAAA      AAAAAAAAAAA  
+  AAAAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAAAA  
+  AAAAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAAAA  
+  AAAAA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AAAAA  
+  AAA       AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA       AAA  
+  AA      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      AA  
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA        
+      AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA      
+</pre>
             <p>⚂ Email</p>
             <p class="text-gray-300">
                 A weekly email round-up to highlight achievements
