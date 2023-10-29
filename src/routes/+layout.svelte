ab741ff138aa79324254808d90c60f66b536dc30
Charlie Coppinger <charles.coppinger@gmail.com>
2023-10-29 13:35:00 +1300

    ship it

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script>
+    import "../app.css";
+    import "@fontsource-variable/source-code-pro";
+</script>
+
+<main
+    class="bg-black text-[#2CFF00] min-h-screen min-w-screen flex justify-center items-center"
+>
+    <slot />
+</main>
