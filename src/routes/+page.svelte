89d0eb4543ddfb17246b472f62ffab0623c06182
Charlie Coppinger <charles.coppinger@gmail.com>
2023-10-30 14:53:04 +1300

    improve accesibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,6 +7,7 @@
 
 <div class="flex flex-col gap-4 md:gap-16 items-center text-center px-8 py-8">
     <pre
+        aria-hidden="true"
         tabindex="-1"
         id="output"
         style="white-space: pre;
@@ -63,25 +64,26 @@
                                                                                         
 </pre>
     <div class="flex flex-col md:flex-row">
-        <pre class="harry" tabindex="-1">
-ooooooooooooo oooo                    
-8'   888   `8 `888                    
-     888       888 .oo.    .ooooo.    
-     888       888P"Y88b  d88' `88b   
-     888       888   888  888ooo888   
-     888       888   888  888    .o   
-    o888o     o888o o888o `Y8bod8P' " 
+        <p class="sr-only">The Gamblers Guild</p>
+        <pre aria-hidden="true" class=" harry" tabindex="-1">
+ooooooooooooo oooo                  
+8'   888   `8 `888                  
+     888       888 .oo.    .ooooo.  
+     888       888P"Y88b  d88' `88b 
+     888       888   888  888ooo888 
+     888       888   888  888    .o 
+    o888o     o888o o888o `Y8bod8P' 
     </pre>
-        <pre class="harry" tabindex="-1">
-        .oooooo.                                 .o8       oooo                             
-       d8P'  `Y8b                               "888       `888                             
-      888            .oooo.   ooo. .oo.  .oo.    888oooo.   888   .ooooo.  oooo d8b  .oooo.o
-      888           `P  )88b  `888P"Y88bP"Y88b   d88' `88b  888  d88' `88b `888""8P d88(  "8
-      888     ooooo  .oP"888   888   888   888   888   888  888  888ooo888  888     `"Y88b. 
-      `88.    .88'  d8(  888   888   888   888   888   888  888  888    .o  888     o.  )88b
-       `Y8bood8P'   `Y888""8o o888o o888o o888o  `Y8bod8P' o888o `Y8bod8P' d888b    8""888P'
+        <pre aria-hidden="true" class=" harry" tabindex="-1">
+        .oooooo.                                 .o8       oooo                               
+       d8P'  `Y8b                               "888       `888                               
+      888            .oooo.   ooo. .oo.  .oo.    888oooo.   888   .ooooo.  oooo d8b  .oooo.o  
+      888           `P  )88b  `888P"Y88bP"Y88b   d88' `88b  888  d88' `88b `888""8P d88(  "8  
+      888     ooooo  .oP"888   888   888   888   888   888  888  888ooo888  888     `"Y88b.   
+      `88.    .88'  d8(  888   888   888   888   888   888  888  888    .o  888     o.  )88b  
+       `Y8bood8P'   `Y888""8o o888o o888o o888o  `Y8bod8P' o888o `Y8bod8P' d888b    8""888P'  
     </pre>
-        <pre class="harry" tabindex="-1">
+        <pre aria-hidden="true" class=" harry" tabindex="-1">
        .oooooo.                 o8o  oooo        .o8
       d8P'  `Y8b                `"'  `888       "888
      888           oooo  oooo  oooo   888   .oooo888
@@ -141,9 +143,7 @@ ooooooooooooo oooo
                     />
 
                     <Dialog.Footer>
-                        <!-- <input type="submit" value="Sign Up" /> -->
                         <Button type="submit" value="Sign Up">Submit</Button>
-                        <!-- <Button type="submit">Save changes</Button> -->
                     </Dialog.Footer>
                 </form>
             </Dialog.Content>
@@ -155,6 +155,7 @@ ooooooooooooo oooo
     >
         <div class="flex flex-col gap-4 items-center w-full">
             <pre
+                aria-hidden="true"
                 style="white-space: pre;
   font-family: monospace;
   font-size: 3px;
@@ -201,6 +202,7 @@ ooooooooooooo oooo
         </div>
         <div class="flex flex-col gap-4 items-center w-full">
             <pre
+                aria-hidden="true"
                 style="white-space: pre;
   font-family: monospace;
   font-size: 3px;
@@ -245,6 +247,7 @@ ooooooooooooo oooo
         </div>
         <div class="flex flex-col gap-4 items-center w-full">
             <pre
+                aria-hidden="true"
                 style="white-space: pre;
   font-family: monospace;
   font-size: 3px;
