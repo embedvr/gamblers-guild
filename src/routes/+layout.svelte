795c814c88d90047077458d95f769c97f433a5e4
Charlie Coppinger <charles.coppinger@gmail.com>
2023-10-30 13:11:41 +1300

    update page title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
     import "@fontsource-variable/source-code-pro";
 </script>
 
+<svelte:head>
+    <title>The Gamblers Guild ⚅</title>
+</svelte:head>
+
 <main class="min-h-screen min-w-screen flex justify-center items-center">
     <slot />
 </main>
