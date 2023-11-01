5ddf6e5702c05ad0f0a18ad0216803287bb05cd5
Charlie Coppinger <charles.coppinger@gmail.com>
2023-11-01 14:01:41 +1300

    get in code of conduct

---
+++ src/lib/components/coc.js
@@ -0,0 +1,81 @@
+export const coc = [
+    {
+        id: 1,
+        title: "Purpose and Scope",
+        summary: "",
+        description:
+            "This code of conduct outlines our expectations for all members of the community. We aim to provide a supportive and inclusive space where everyone can share, learn, and grow. By joining our community, you agree to adhere to these guidelines.",
+    },
+    {
+        id: 2,
+        title: "No Politics",
+        summary:
+            "There are many outlets for discussing politics on the Internet, this is not one of them.",
+        description:
+            "To maintain focus and prevent divisive conversations, please refrain from discussing politics within the community. Our primary aim is to support one another in our entrepreneurial journeys, and political debates can detract from this purpose.",
+    },
+    {
+        id: 3,
+        title: "Inclusivity and Tolerance",
+        summary: "Inclusivity and mutual respect is important.",
+        description:
+            "We value the diverse backgrounds, experiences, and perspectives of our members. Discrimination, harassment, or exclusion based on race, color, religion, gender, gender identity, nationality, disability, sexual orientation, or age is strictly prohibited.",
+    },
+    {
+        id: 4,
+        title: "Respectful Interpretation Bias",
+        summary: "Assume good intent.",
+        description:
+            "Always assume the most respectful interpretation of what others are saying. Misunderstandings can arise from cultural or linguistic differences. Before reacting, seek clarification and give the benefit of the doubt.",
+    },
+    {
+        id: 5,
+        title: "Constructive Feedback",
+        summary:
+            "Well-intentioned feedback is great, personal attacks are un-accpetable.",
+        description:
+            "If offering feedback or criticism, ensure it's constructive and well-intentioned. Avoid personal attacks, and focus on the content or idea rather than the individual.",
+    },
+    {
+        id: 6,
+        title: "Don't Sell",
+        summary: "Self Promotion is fine, selling isn't.",
+        description:
+            "While sharing your projects or milestones is encouraged, blatant self-promotion, spamming, or advertising without relevant context is not allowed.",
+    },
+    {
+        id: 7,
+        title: "Privacy",
+        summary: "Default to private, seek permission before sharing.",
+        description:
+            "Respect the privacy of others. Do not share or distribute personal information of any community member without their explicit consent.",
+    },
+    {
+        id: 8,
+        title: "Avoid Sensitive Topics",
+        summary: "Sensitive topics are best avoided.",
+        description:
+            "Apart from politics, avoid discussions on sensitive topics that can easily lead to heated debates or offend others, such as religion or personal beliefs. Our focus is on entrepreneurship and mutual support.",
+    },
+    {
+        id: 9,
+        title: "Report Issues",
+        summary: "Please report issues, we'll actively deal with them.",
+        description:
+            "If you encounter or observe behavior that violates this code of conduct, please report it to the community moderators or administrators. We'll address it confidentially and promptly.",
+    },
+    {
+        id: 10,
+        title: "Consequences",
+        summary: "Fucketh around, findeth out.",
+        description:
+            "Violations of this code can result in warnings, temporary suspensions, or permanent bans, depending on the severity and frequency of the infractions.",
+    },
+    {
+        id: 11,
+        title: "Continuous Improvement",
+        summary: "Help us grow the garden.",
+        description:
+            "We believe in continuous improvement. If you have suggestions to enhance the community or this code of conduct, we are open to feedback.",
+    },
+];
