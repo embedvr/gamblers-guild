3b23ea56c5318f22106de1563efba335e10d432c
Charlie Coppinger <charles.coppinger@gmail.com>
2023-11-03 12:09:05 +1300

    add invite catch-all route and typeform embed

---
+++ src/routes/invite/[slug]/+page.svelte
@@ -0,0 +1,12 @@
+<svelte:head>
+    <script src="https://embed.typeform.com/next/embed.js"></script>
+</svelte:head>
+
+<div
+    data-tf-widget="p5LtH4lD"
+    data-tf-opacity="100"
+    data-tf-iframe-props="title=Gamblers Guild Invite"
+    data-tf-transitive-search-params
+    data-tf-medium="snippet"
+    style="width:100%;height:500px;"
+/>
