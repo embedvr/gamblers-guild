0987106da00423c946d1f8ab4f8230cf4d80e279
embed <brydon@helium.email>
2023-11-03 00:28:13 -0400

    [~] minimal layout when on invite page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,81 +1,84 @@
 <script>
-    import "../app.css";
-    import "@fontsource-variable/source-code-pro";
-    import { PlausibleAnalytics } from "@accuser/svelte-plausible-analytics";
+  import "../app.css";
+  import "@fontsource-variable/source-code-pro";
+  import { PlausibleAnalytics } from "@accuser/svelte-plausible-analytics";
+  import { page } from "$app/stores";
 </script>
 
 <svelte:head>
-    <title>The Gamblers Guild ⚅</title>
+  <title>The Gamblers Guild ⚅</title>
 
-    <meta name="title" content="The Gamblers Guild ⚅" />
-    <meta
-        name="description"
-        content="A collective of founders & indie hackers making small bets & supporting each other"
-    />
+  <meta name="title" content="The Gamblers Guild ⚅" />
+  <meta
+    name="description"
+    content="A collective of founders & indie hackers making small bets & supporting each other"
+  />
 
-    <!-- Open Graph / Facebook -->
-    <meta property="og:type" content="website" />
-    <meta property="og:url" content="https://www.thegamblersguild.com/" />
-    <meta property="og:title" content="The Gamblers Guild ⚅" />
-    <meta
-        property="og:description"
-        content="A collective of founders & indie hackers making small bets & supporting each other"
-    />
-    <meta property="og:image" content="/meta-cover.jpg" />
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://www.thegamblersguild.com/" />
+  <meta property="og:title" content="The Gamblers Guild ⚅" />
+  <meta
+    property="og:description"
+    content="A collective of founders & indie hackers making small bets & supporting each other"
+  />
+  <meta property="og:image" content="/meta-cover.jpg" />
 
-    <!-- Twitter -->
-    <meta property="twitter:card" content="summary_large_image" />
-    <meta property="twitter:url" content="https://www.thegamblersguild.com/" />
-    <meta property="twitter:title" content="The Gamblers Guild ⚅" />
-    <meta
-        property="twitter:description"
-        content="A collective of founders & indie hackers making small bets & supporting each other"
-    />
-    <meta property="twitter:image" content="/meta-cover.jpg" />
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://www.thegamblersguild.com/" />
+  <meta property="twitter:title" content="The Gamblers Guild ⚅" />
+  <meta
+    property="twitter:description"
+    content="A collective of founders & indie hackers making small bets & supporting each other"
+  />
+  <meta property="twitter:image" content="/meta-cover.jpg" />
 </svelte:head>
 
 <PlausibleAnalytics />
 
-<header class="w-full flex justify-center py-12">
+{#if !String($page.url.pathname).startsWith("/invite")}
+  <header class="w-full flex justify-center py-12">
     <ul class="flex justify-between max-w-md gap-4 sm:gap-12">
-        <li><a class="underline underline-offset-4" href="/">Home</a></li>
-        <li>
-            <a class="underline underline-offset-4" href="/coc"
-                >Code of Conduct</a
-            >
-        </li>
-        <li>
-            <a
-                class="underline underline-offset-4"
-                href="https://twitter.com/gamblersguild"
-                target="_blank">Twitter</a
-            >
-            ↗
-        </li>
+      <li><a class="underline underline-offset-4" href="/">Home</a></li>
+      <li>
+        <a class="underline underline-offset-4" href="/coc">Code of Conduct</a>
+      </li>
+      <li>
+        <a
+          class="underline underline-offset-4"
+          href="https://twitter.com/gamblersguild"
+          target="_blank">Twitter</a
+        >
+        ↗
+      </li>
     </ul>
-</header>
+  </header>
+{/if}
 
 <main
-    class="min-h-screen min-w-screen flex justify-center items-center overflow-x-hidden"
+  class="min-h-screen min-w-screen flex justify-center items-center overflow-x-hidden"
 >
-    <slot />
+  <slot />
 </main>
 
-<footer class="flex flex-col gap-16 items-center justify-center my-8">
+{#if !String($page.url.pathname).startsWith("/invite")}
+  <footer class="flex flex-col gap-16 items-center justify-center my-8">
     <div class="flex flex-col gap-2 items-center">
-        <p>⚀ ⚁ ⚂ ⚃ ⚄ ⚅</p>
-        <p class="text-gray-300">
-            Audentes Fortuna Iuvat // Fortune favors the bold
-        </p>
+      <p>⚀ ⚁ ⚂ ⚃ ⚄ ⚅</p>
+      <p class="text-gray-300">
+        Audentes Fortuna Iuvat // Fortune favors the bold
+      </p>
     </div>
     <div class="flex gap-2">
-        <p>Credits:</p>
-        <p class="text-gray-300">
-            Glitch animation by <a
-                href="https://twitter.com/harry_webb98"
-                target="_blank">Harry</a
-            >
-        </p>
+      <p>Credits:</p>
+      <p class="text-gray-300">
+        Glitch animation by <a
+          href="https://twitter.com/harry_webb98"
+          target="_blank">Harry</a
+        >
+      </p>
     </div>
     <a href="https://twitter.com/gamblersguild" target="_blank">𝕏</a>
-</footer>
+  </footer>
+{/if}
--- src/routes/invite/[slug]/+page.svelte
+++ src/routes/invite/[slug]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-    let form: HTMLDivElement;
-    import { createWidget } from "@typeform/embed";
-    import "@typeform/embed/build/css/widget.css";
-    import { onMount } from "svelte";
+  let form: HTMLDivElement;
+  import { createWidget } from "@typeform/embed";
+  import "@typeform/embed/build/css/widget.css";
+  import { onMount } from "svelte";
 
-    onMount(() => {
-        createWidget("p5LtH4lD", {
-            container: form,
-        });
+  onMount(() => {
+    createWidget("p5LtH4lD", {
+      container: form,
     });
+  });
 </script>
 
 <div bind:this={form} style="width: 100%; height: 500px;" />
